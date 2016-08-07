481141e308e52319b09d035eed63f38b5c29f92f
Einar Lielmanis <einars@gmail.com>
2016-08-07 12:31:18 +0300

    Update link to problems

--- problems/index.php
+++ problems/index.php
@@ -4,8 +4,8 @@ echo '<table>';
 echo '<tr><td>NPK</td><td>Full size</td><td>Actual size</td></tr>';
 for ($i=1; $i <= 101; $i++) {
     printf('<tr><th><a href="%s">%s</a></th><td><img src="%s"></td><td><img src="%s"></td></tr>'
-        , sprintf('./%03d.txt', $i)
-        , sprintf('%03d', $i)
+        , sprintf('../all-problems/%04d.txt', $i)
+        , sprintf('%04d', $i)
         , sprintf('./fit-%03d.png', $i)
         , sprintf('./nofit-%03d.png', $i)
     );

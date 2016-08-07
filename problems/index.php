43883d2bd5177e781d2eed5f6a4b053507eaec33
Einar Lielmanis <einars@gmail.com>
2016-08-07 12:37:58 +0300

    Fix links again

--- problems/index.php
+++ problems/index.php
@@ -4,8 +4,8 @@ echo '<table>';
 echo '<tr><td>NPK</td><td>Full size</td><td>Actual size</td></tr>';
 for ($i=1; $i <= 101; $i++) {
     printf('<tr><th><a href="%s">%s</a></th><td><img src="%s"></td><td><img src="%s"></td></tr>'
-        , sprintf('../all-problems/%04d.txt', $i)
-        , sprintf('%04d', $i)
+        , sprintf('../all-problems/%05d.txt', $i)
+        , sprintf('%05d', $i)
         , sprintf('./fit-%03d.png', $i)
         , sprintf('./nofit-%03d.png', $i)
     );
